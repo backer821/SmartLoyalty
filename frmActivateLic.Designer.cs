d45c524c1ba1a6afe7c9407b51a85fb37cec83e2
backer821 <43517396+backer821@users.noreply.github.com>
2018-09-23 19:10:54 +0530

    test chndes
    test

--- frmActivateLic.Designer.cs
+++ frmActivateLic.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(115, 40);
+            this.button2.Location = new System.Drawing.Point(93, 51);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(228, 67);
             this.button2.TabIndex = 1;
@@ -44,7 +44,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(115, 134);
+            this.button1.Location = new System.Drawing.Point(93, 176);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(228, 50);
             this.button1.TabIndex = 2;
@@ -54,7 +54,7 @@
             // 
             // frmActivateLic
             // 
-            this.ClientSize = new System.Drawing.Size(654, 350);
+            this.ClientSize = new System.Drawing.Size(442, 350);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
             this.Name = "frmActivateLic";
